77209fd82415312a7aefd201a6ee9be452b225da
iFelix18 <19800006+iFelix18@users.noreply.github.com>
2019-02-23 15:09:03 +0100

    Dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,128 +1,46 @@
-"use strict";
-//============================================================================//
-var chalk = require("chalk");
-var gulp = require("gulp");
-var autoprefixer = require("gulp-autoprefixer");
-var beautifyCode = require("gulp-beautify-code");
-var bump = require("gulp-bump");
-var gfi = require("gulp-file-insert");
-var rename = require("gulp-rename");
-var sass = require("gulp-sass");
-var sassLint = require("gulp-sass-lint");
-var timestamp = require("time-stamp");
+'use strict'
 
-sass.compiler = require("node-sass");
+let autoprefixer = require('gulp-autoprefixer')
+let gulp = require('gulp')
+let insert = require('gulp-file-insert')
+let rename = require('gulp-rename')
+let sass = require('gulp-sass')
 
-var log = console.log;
-var time = "[HH:mm:ss]";
-var end = chalk.green.bold.inverse;
-var error = chalk.red.bold.inverse(timestamp(time) + " Something went wrong!")
-//============================================================================//
-//================================BUMP VERSION================================//
-// bump to a major update (1.0.0)
-gulp.task("bump:major", () =>
-    gulp.src(["./package.json", "./css/theme.user.css", "./sass/**/_version.scss"])
-    .pipe(bump({
-        type: "major"
-    }))
-    .pipe(gulp.dest(function (file) {
-        return file.base;
-    }))
-);
-// bump to a minor update (0.1.0)
-gulp.task("bump:minor", () =>
-    gulp.src(["./package.json", "./css/theme.user.css", "./sass/**/_version.scss"])
-    .pipe(bump({
-        type: "minor"
-    }))
-    .pipe(gulp.dest(function (file) {
-        return file.base;
-    }))
-);
-// bump to a patched update (0.0.2)
-gulp.task("bump:patch", () =>
-    gulp.src(["./package.json", "./css/theme.user.css", "./sass/**/_version.scss"])
-    .pipe(bump({
-        type: "patch"
-    }))
-    .pipe(gulp.dest(function (file) {
-        return file.base;
-    }))
-);
-// bump to a prerelease update (0.0.1-2)
-gulp.task("bump:prerelease", () =>
-    gulp.src(["./package.json", "./css/theme.user.css", "./sass/**/_version.scss"])
-    .pipe(bump({
-        type: "prerelease"
-    }))
-    .pipe(gulp.dest(function (file) {
-        return file.base;
-    }))
-);
-//============================================================================//
-//====================================CSS=====================================//
-gulp.task("compile", () =>
-    gulp.src("./sass/**/*.scss")
-    .pipe(beautifyCode({
-        newline_between_rules: true
-    }))
-    .pipe(sassLint())
-    .pipe(sassLint.format())
-    .pipe(sassLint.failOnError())
-    .pipe(sass({
-        outputStyle: "expanded",
-        indentType: "space",
-        indentWidth: 4
-    }))
-    .pipe(gulp.dest("./css"))
-    .on("end", () =>
-        log(end(timestamp(time) + " Compiled!"))
-    )
-    .on("error", () =>
-        log(error)
-    )
-);
-gulp.task("autoprefix", () =>
-    gulp.src("./css/theme.css")
+sass.compiler = require('node-sass')
+
+gulp.task('autoprefix', function () {
+  return gulp.src('./css/theme.css')
     .pipe(autoprefixer({
-        browsers: [
-            "> 0.2%",
-            "last 2 versions"
-        ],
-        cascade: false
+      browsers: [
+        '> 0.2%',
+        'last 2 versions'
+      ],
+      cascade: false
     }))
-    .pipe(gulp.dest("./css"))
-    .on("end", () =>
-        log(end(timestamp(time) + " Autoprefixed!"))
-    )
-    .on("error", () =>
-        log(error)
-    )
-);
-//============================================================================//
-//==================================USERCSS===================================//
-gulp.task("build", () =>
-    gulp.src("./css/theme.user.css")
-    .pipe(gfi({
-        "/* theme */": "./css/theme.css"
+    .pipe(gulp.dest('./css'))
+})
+
+gulp.task('usercss', function () {
+  return gulp.src('./css/theme.user.css')
+    .pipe(insert({
+      '/* theme */': './css/theme.css'
     }))
-    .pipe(beautifyCode({
-        newline_between_rules: true
-    }))
-    .pipe(rename("darkt.user.css"))
-    .pipe(gulp.dest("./"))
-    .on("end", () =>
-        log(end(timestamp(time) + " Builded!"))
-    )
-    .on("error", () =>
-        log(error)
-    )
-)
-//============================================================================//
-gulp.task("sass:watch", () =>
-    gulp.watch("./sass/**/*.scss", gulp.series("compile", "autoprefix", "build"))
-);
-//============================================================================//
-gulp.task("watch", gulp.parallel("sass:watch"));
-gulp.task("default", gulp.series("compile", "autoprefix", "build", "watch"));
-//============================================================================//
+    .pipe(rename('darkt.user.css'))
+    .pipe(gulp.dest('./'))
+})
+
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass({
+      outputStyle: 'expanded',
+      indentType: 'space',
+      indentWidth: 2
+    }).on('error', sass.logError))
+    .pipe(gulp.dest('./css'))
+})
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', gulp.series('sass', 'autoprefix', 'usercss'))
+})
+
+gulp.task('default', gulp.series('sass:watch'))
